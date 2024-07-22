b75eeacf4be4e6d6ea63bf5a602d689121431f68
nihalnouha <nihalnouha>
2024-07-22 21:16:11 +0100

    first learning typescript

--- sandbox.ts
+++ sandbox.ts
@@ -7,3 +7,69 @@
 // inputs.forEach(input => {
 //   console.log(input);
 // });
+
+
+//lesson3
+
+// let character = 'mario';
+// let age = 30;
+// let isBlackBelt = false;
+
+// character = 'luigi';
+
+
+// age = 40;
+
+// isBlackBelt = true;
+
+// const circ = (diameter: number) => {
+//   return diameter * Math.PI;
+// };
+
+// console.log(circ(7.5));
+
+
+
+//lesson4
+
+// arrays
+let names = ['luigi', 'mario', 'yoshi'];
+
+names.push('toad');
+console.log(names);
+
+//  names.push(3);
+
+// names[1] = 3;
+
+let numbers = [10, 20, 12, 15];
+
+numbers.push(25);
+console.log(numbers)
+// numbers.push('shaun');
+// numbers[0] = 'shaun';
+
+let mixed = ['ken', 4, 'chun-li', 8, 9];
+
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
+
+// objects
+let ninja = {
+  name: 'mario',
+  belt: 'black',
+  age: 30
+};
+
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age = '30';
+// ninja.skills = ['fighting', 'sneaking']
+
+ninja = {
+  name: 'yoshi',
+  belt: 'orange',
+  age: 40,
+  // skills: ['running'],
+};
