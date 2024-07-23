bec6c6ebe2f444f577a32c1012b9f328835a779c
nihalnouha <nihalnouha>
2024-07-23 14:30:57 +0100

    new version

--- sandbox.js
+++ sandbox.js
@@ -17,36 +17,88 @@
 // console.log(circ(7.5));
 //lesson4
 // arrays
-var names = ['luigi', 'mario', 'yoshi'];
-names.push('toad');
-console.log(names);
+// let names = ['luigi', 'mario', 'yoshi'];
+// names.push('toad');
+// console.log(names);
 //  names.push(3);
 // names[1] = 3;
-var numbers = [10, 20, 12, 15];
-numbers.push(25);
-console.log(numbers);
+// let numbers = [10, 20, 12, 15];
+// numbers.push(25);
+// console.log(numbers)
 // numbers.push('shaun');
 // numbers[0] = 'shaun';
-var mixed = ['ken', 4, 'chun-li', 8, 9];
-mixed.push('ryu');
-mixed.push(10);
-mixed[0] = 3;
+// let mixed = ['ken', 4, 'chun-li', 8, 9];
+// mixed.push('ryu');
+// mixed.push(10);
+// mixed[0] = 3;
 // objects
-var ninja = {
-    name: 'mario',
-    belt: 'black',
-    age: 30
-};
-ninja.age = 40;
-ninja.name = 'ryu';
-console.log(ninja);//ninja.age is changed to 40 and ninja.name also changed
-
+// let ninja = {
+//   name: 'mario',
+//   belt: 'black',
+//   age: 30
+// };
+// ninja.age = 40;
+// ninja.name = 'ryu';
 // ninja.age = '30';
 // ninja.skills = ['fighting', 'sneaking']
-ninja = {
-    name: 'yoshi',
-    belt: 'orange',
-    age: 40,
-    // skills: ['running'],
-};
-console.log(ninja);
+// ninja = {
+//   name: 'yoshi',
+//   belt: 'orange',
+//   age: 40,
+//   // skills: ['running'],
+// };
+// lesson5
+// let character: string = 'mario';
+// let age: number;
+// let isLoggedIn: boolean;
+// age = 'luigi';ERREUR
+// age = 30;
+// isLoggedIn = 25;ERREUR
+//isLoggedIn = true;
+// arrays
+// let ninjas: string[] = [];
+// ninjas.push('ryu');
+// ninjas.push('chun-li');
+// console.log(ninjas);
+// union types
+// let mixed: (string|number|boolean)[] = [];
+// mixed.push('hello');
+// mixed.push(false);
+// mixed.push(20);
+// console.log(mixed);
+// let uid: string|number;
+// objects
+// let ninjaOne: object;
+// ninjaOne =
+// { name: 'yoshi',
+//  age: 30 };
+// let ninjaTwo: {
+//   name: string,
+//   age: number,
+//   beltColour: string
+// };
+// ninjaTwo = { name: 'ken', age: 20, beltColour: 'black' };
+
+
+//lesson6
+
+// var age = 25;
+// age = true;
+// console.log(age);
+// age = 'hello';
+// console.log(age);
+// age = { name: 'luigi' };
+// console.log(age);
+// var mixed = [];
+// mixed.push(5);
+// mixed.push('mario');
+// mixed.push(false);
+// console.log(mixed);
+// var ninja;
+// ninja = { name: 'yoshi', age: 25 };
+// console.log(ninja);
+// ninja = { name: 25, age: 'yoshi' };
+// console.log(ninja);
+
+
+//LESSON7
--- sandbox.ts
+++ sandbox.ts
@@ -33,43 +33,120 @@
 //lesson4
 
 // arrays
-let names = ['luigi', 'mario', 'yoshi'];
+// let names = ['luigi', 'mario', 'yoshi'];
 
-names.push('toad');
-console.log(names);
+// names.push('toad');
+// console.log(names);
 
 //  names.push(3);
 
 // names[1] = 3;
 
-let numbers = [10, 20, 12, 15];
+// let numbers = [10, 20, 12, 15];
 
-numbers.push(25);
-console.log(numbers)
+// numbers.push(25);
+// console.log(numbers)
 // numbers.push('shaun');
 // numbers[0] = 'shaun';
 
-let mixed = ['ken', 4, 'chun-li', 8, 9];
+// let mixed = ['ken', 4, 'chun-li', 8, 9];
 
-mixed.push('ryu');
-mixed.push(10);
-mixed[0] = 3;
+// mixed.push('ryu');
+// mixed.push(10);
+// mixed[0] = 3;
 
 // objects
-let ninja = {
-  name: 'mario',
-  belt: 'black',
-  age: 30
-};
+// let ninja = {
+//   name: 'mario',
+//   belt: 'black',
+//   age: 30
+// };
 
-ninja.age = 40;
-ninja.name = 'ryu';
+// ninja.age = 40;
+// ninja.name = 'ryu';
 // ninja.age = '30';
 // ninja.skills = ['fighting', 'sneaking']
 
-ninja = {
-  name: 'yoshi',
-  belt: 'orange',
-  age: 40,
-  // skills: ['running'],
-};
+// ninja = {
+//   name: 'yoshi',
+//   belt: 'orange',
+//   age: 40,
+//   // skills: ['running'],
+// };
+
+
+// lesson5
+
+// let character: string = 'mario';
+// let age: number;
+// let isLoggedIn: boolean;
+
+// age = 'luigi';ERREUR
+// age = 30;
+
+// isLoggedIn = 25;ERREUR
+//isLoggedIn = true;
+
+// arrays
+
+// let ninjas: string[] = [];
+
+// ninjas.push('ryu');
+// ninjas.push('chun-li');
+// console.log(ninjas);
+
+// union types
+
+// let mixed: (string|number|boolean)[] = [];
+// mixed.push('hello');
+// mixed.push(false);
+// mixed.push(20);
+// console.log(mixed);
+
+// let uid: string|number;
+
+// objects
+
+// let ninjaOne: object;
+// ninjaOne =
+// { name: 'yoshi',
+//  age: 30 };
+
+// let ninjaTwo: {
+//   name: string,
+//   age: number,
+//   beltColour: string
+// };
+// ninjaTwo = { name: 'ken', age: 20, beltColour: 'black' };
+
+
+
+//lesson6
+
+// let age: any = 25;
+
+// age = true;
+// console.log(age);
+// age = 'hello';
+// console.log(age);
+// age = { name: 'luigi' };
+// console.log(age);
+
+// let mixed: any[] = [];
+
+// mixed.push(5);
+// mixed.push('mario');
+// mixed.push(false);
+// console.log(mixed);
+
+// let ninja: { name: any, age: any };
+
+// ninja = { name: 'yoshi', age: 25 };
+// console.log(ninja);
+
+// ninja = { name: 25, age: 'yoshi' };
+// console.log(ninja);
+
+
+
+//lesson7
